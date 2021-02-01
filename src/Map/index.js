0d403f0f9970c94ce368c8215083e73c1846455b
Kamil <kcassamchenai@matchbox.fr>
2021-02-01 15:53:57 +0100

    event emitter with bounds value on bounds changed

--- src/Map/index.js
+++ src/Map/index.js
@@ -5,32 +5,19 @@ import { MapListContext } from "../MapListContext"
 import { Root } from "./styles"
 import { isBrowser } from "../utils"
 import { sizes } from "../style/Breakpoint"
-//import { RoundedIcon } from "../Icon/RoundedIcon"
-//<RoundedIcon key={item.dealerId} text={item.nameTranslated} lat={item.location.lat} lng={item.location.lng} type="location" onClick={() => selectItem(item, "map")}/>
 
-/*const long;
-cont lati;
-if(navigator.geolocation)
-{
-    navigator.geolocation.getCurrentPosition(
-        (position) => {
-            lati = position.coords.latitude;
-            long = position.coords.longitude;
-        }
-    )
-};*/
 const mobileMaxWidth = `${sizes.xs.max}px`
-
+const googleMapsApiKey = "AIzaSyDLBYFZ20ju_3sSKYtbjolEscpnPGGuVnI"
 const Map = () => {
     const { retailers, selectedRetailer, selectItem, displayLoader } = useContext(MapListContext)
-    const refCenter = useRef()
 
     // Load the Google maps scripts
-    const { isLoaded } = useLoadScript({
-        googleMapsApiKey: "AIzaSyDLBYFZ20ju_3sSKYtbjolEscpnPGGuVnI",
-    })
+    const { isLoaded } = useLoadScript({ googleMapsApiKey })
 
+    // State to store the map instance
     const [map, setMap] = React.useState(null)
+
+    // Store the map isntance in the state
     const onLoad = useCallback(
         (map) => {
             setMap(map)
@@ -38,6 +25,10 @@ const Map = () => {
         [setMap]
     )
 
+    // Using a ref to store center of the map when it's change so the map don't move back
+    const refCenter = useRef()
+
+    // Fitbounds allow to adapt the map center and zoom to disply all the injected markers
     useEffect(() => {
         if (map && retailers.length) {
             const bounds = new window.google.maps.LatLngBounds()
@@ -49,6 +40,7 @@ const Map = () => {
         }
     }, [map, retailers])
 
+    // Detect the breadking change of the design that need to change the elements in the dom
     const [isMobile, setIsMobile] = useState(false)
 
     useEffect(() => {
@@ -67,14 +59,35 @@ const Map = () => {
         }
     }, [])
 
+    // Update the center ref on center change to prevent the map from moving on unselec
     const handleCenterChange = useCallback(() => {
         const newCenter = map.getCenter()
         refCenter.current = newCenter.toJSON()
     })
 
+    // Tmp for test / dev purpose
     const handleZoomChanged = useCallback(() => {
-        if (map) console.log(map.getBounds())
+        if (map) console.log(map.getZoom())
     })
+
+    const handleBoundsChanged = useCallback(() => {
+        if (map) {
+            let ne = map.getBounds().getNorthEast()
+            let sw = map.getBounds().getSouthWest()
+            //console.log(ne.lat() + ";" + ne.lng())
+            //console.log(sw.lat() + ";" + sw.lng())
+            //console.log(map.getBounds().toJSON())
+            var event = new CustomEvent("mapBoundsChanged ", { detail: map.getBounds().toJSON() })
+            window.dispatchEvent(event)
+        }
+    })
+
+    /*useEffect(() => {
+        if (isBrowser) {
+            window.addEventListener("mapBoundsChanged", handleRedMark)
+            return () => window.removeEventListener("mapBoundsChanged", setHasFavorite)
+        }
+    }, [])*/
 
     const onMapClick = useCallback((e) => {
         if (e.target === e.currentTarget) {
@@ -98,6 +111,8 @@ const Map = () => {
                     onCenterChanged={handleCenterChange}
                     onClick={onMapClick}
                     onZoomChanged={handleZoomChanged}
+                    onBoundsChanged={handleBoundsChanged}
+                    clickableIcons={true}
                 >
                     {!!retailers.length &&
                         retailers.map((item) => {
